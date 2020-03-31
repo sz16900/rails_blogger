7c5fb25c2babbf3d7c7fdb9c4303b5695b2c1389
sz16900 <sz16900@my.bristol.ac.uk>
2020-03-31 14:52:30 -0500

    :art: add controller
    mvc framework

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,14 @@
+class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+      end
+    def show
+        @article = Article.find(params[:id])
+    end
+    def new
+        @article = Article.new
+    end
+    def create 
+        
+    end
+end
